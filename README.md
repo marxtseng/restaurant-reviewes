a6719ece7668538db0a23fd96db165f8d9786b96
Marx Tseng <marx.zeng@gmail.com>
2016-09-26 08:31:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Restaurant Reviewes
+
+
+
+## Install the Polymer-CLI
+
+First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) installed. Then run `polymer serve` to serve your application locally.
+
+## Install the bower package & node package
+
+```
+$ npm install -g bower
+$ bower install
+$ npm install
+```
+
+## Viewing Your Application
+
+```
+$ polymer serve
+```
+
+## Building & Tests
+
+```
+$ gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+var vulcanize = require('gulp-vulcanize');
+var crisper = require('gulp-crisper');
+var connect = require('gulp-connect');
+var shell = require('gulp-shell');
+var minifyInline = require('gulp-minify-inline');
+
+gulp.task('build', function() {
+    return gulp.src('restaurant-reviewes.html')
+        .pipe(replace(/<link rel="import" href="../g, '<link rel="import" href="../bower_components'))
+        .pipe(gulp.dest('dist'))
+        .pipe(replace(/<script src="../g, '<script src="../bower_components'))
+        .pipe(gulp.dest('dist'))
+        .pipe(vulcanize({
+            inlineScripts: true,
+            inlineCss: true,
+            stripComments: true
+        }))
+        .pipe(minifyInline())
+        .pipe(crisper())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', function() {
+    connect.server({
+        root: 'dist'
+    });
+});
+
+gulp.task('default', ['serve', 'build'], shell.task([
+    /^win/.test(require('os').platform()) ? 'start http://localhost:8080/' : 'open http://localhost:8080/'
+]));
