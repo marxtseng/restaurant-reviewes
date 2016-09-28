0f72f4a9bd8445ccaf4ff5e508f4f1e26ec2417a
Marx Tseng <marx.zeng@gmail.com>
2016-09-28 11:45:16 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var shell = require('gulp-shell');
 var minifyInline = require('gulp-minify-inline');
 
 gulp.task('build', function() {
+    gulp.src('temp/*')
+        .pipe(gulp.dest('dist'));
+        
     return gulp.src('restaurant-reviewes.html')
         .pipe(replace(/<link rel="import" href="../g, '<link rel="import" href="../bower_components'))
         .pipe(gulp.dest('dist'))
